37defa5df2ca5ed3315108fff99a60d351bcb26f
Manan Gaur <manangaurdev27@gmail.com>
2025-07-18 16:40:04 +0200

    Task-2:- Training my Linear Regression model for predicting the house prices and  Visualizing the regression line on the scatter plot and Predicting prices for new areas and save to CSV

--- 1.ML_Codebasics/1.Linear_Regression/Linear_Regression_Single_Variables.py
+++ 1.ML_Codebasics/1.Linear_Regression/Linear_Regression_Single_Variables.py
@@ -18,3 +18,23 @@ df
 plt.xlabel('area(sqr ft)')
 plt.ylabel('price(US$)')
 plt.scatter(df.area, df.price, color='red', marker='+')
+
+
+# Task-2:- Training my Linear Regression model for predicting the house prices
+reg = linear_model.LinearRegression()
+reg.fit(df[['area']], df['price'])
+
+reg.predict([[3300]])
+
+reg.coef_
+
+reg.intercept_
+
+
+d = pd.read_csv('area.csv')
+
+p = reg.predict(d[['area']])
+
+d['prices'] = p
+
+d.to_csv("predicted_prices.csv")
