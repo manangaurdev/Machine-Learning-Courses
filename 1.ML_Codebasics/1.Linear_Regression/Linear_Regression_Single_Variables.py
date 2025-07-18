a3b12faddb8f7b455c077cc9a27d1e27ba06bc9a
Manan Gaur <manangaurdev27@gmail.com>
2025-07-18 15:30:57 +0200

    Task-0:- Imported necessary python packages and required homeprices.csv file

---
+++ 1.ML_Codebasics/1.Linear_Regression/Linear_Regression_Single_Variables.py
@@ -0,0 +1,10 @@
+"""
+Machine Learning is a set of techniques to make computer better at doing things that
+humans (traditionally) can do better than machines
+
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
