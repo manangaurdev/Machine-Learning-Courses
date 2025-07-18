43d330a444454c8ea1fc8096808b54ea0fc960eb
Manan Gaur <manangaurdev27@gmail.com>
2025-07-18 15:40:03 +0200

    Task-1:- Analyzed the given dataset and plotted the relationship between them

--- 1.ML_Codebasics/1.Linear_Regression/Linear_Regression_Single_Variables.py
+++ 1.ML_Codebasics/1.Linear_Regression/Linear_Regression_Single_Variables.py
@@ -3,8 +3,18 @@ Machine Learning is a set of techniques to make computer better at doing things 
 humans (traditionally) can do better than machines
 
 """
+# Task-0:- Importing necessay packages and required "homeprices.csv" file
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
+df = pd.read_csv("homeprices.csv")
+df
+
+
+# Task-1:- Analysing the dataset and understanding the relationship between 'area' and 'price' of the house
+plt.xlabel('area(sqr ft)')
+plt.ylabel('price(US$)')
+plt.scatter(df.area, df.price, color='red', marker='+')
